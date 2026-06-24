468dbe2b98af51a51928c8682038d5c21d845fb5
88venom14 <artem.chernakov.07@mail.ru>
2026-06-24 21:21:24 +0300

    Update

--- coffee-shop-objects.sql
+++ coffee-shop-objects.sql
@@ -1,20 +1,5 @@
--- =====================================================================
--- Программные объекты базы данных coffee_shop_db
--- Дисциплина МДК.11.01 - Технология разработки и защиты баз данных
--- Курсовой проект "Разработка базы данных для кофейни"
--- Кодировка: UTF-8
---
--- Выполнять после создания таблиц (dump-coffee-shop.sql).
--- Содержит: представление, пользовательскую функцию, триггер,
--- хранимую процедуру (транзакция + переменные + условие + обработчик
--- исключений) и роли доступа.
--- =====================================================================
-
 USE coffee_shop_db;
 
--- ---------------------------------------------------------------------
--- 1. Представление (VIEW): витрина заказов с именами клиентов
--- ---------------------------------------------------------------------
 DROP VIEW IF EXISTS v_order_summary;
 CREATE VIEW v_order_summary AS
 SELECT o.OrderID,
@@ -25,9 +10,6 @@ SELECT o.OrderID,
 FROM customerorders o
     LEFT JOIN customers c ON o.CustomerID = c.CustomerID;
 
--- ---------------------------------------------------------------------
--- 2. Пользовательская функция (FUNCTION): сумма заказа по его позициям
--- ---------------------------------------------------------------------
 DROP FUNCTION IF EXISTS fn_order_total;
 DELIMITER //
 CREATE FUNCTION fn_order_total(p_order_id INT)
@@ -44,9 +26,6 @@ BEGIN
 END //
 DELIMITER ;
 
--- ---------------------------------------------------------------------
--- 3. Триггер (TRIGGER): автопересчёт суммы заказа при добавлении позиции
--- ---------------------------------------------------------------------
 DROP TRIGGER IF EXISTS trg_recalc_total;
 DELIMITER //
 CREATE TRIGGER trg_recalc_total
@@ -59,11 +38,6 @@ BEGIN
 END //
 DELIMITER ;
 
--- ---------------------------------------------------------------------
--- 4. Хранимая процедура (PROCEDURE): оплата заказа.
---    Демонстрирует транзакцию, локальные переменные, условие и
---    обработчик исключений.
--- ---------------------------------------------------------------------
 DROP PROCEDURE IF EXISTS sp_pay_order;
 DELIMITER //
 CREATE PROCEDURE sp_pay_order(
@@ -102,20 +76,11 @@ BEGIN
 END //
 DELIMITER ;
 
--- ---------------------------------------------------------------------
--- 5. Роли и разграничение доступа
--- ---------------------------------------------------------------------
 CREATE ROLE IF NOT EXISTS 'coffee_admin', 'coffee_cashier';
 
--- администратор: полный доступ ко всей базе
 GRANT ALL PRIVILEGES ON coffee_shop_db.* TO 'coffee_admin';
 
--- кассир: только то, что нужно для работы с заказами
 GRANT SELECT ON coffee_shop_db.menu TO 'coffee_cashier';
 GRANT SELECT, INSERT, UPDATE ON coffee_shop_db.customerorders TO 'coffee_cashier';
 GRANT SELECT, INSERT ON coffee_shop_db.orderdetails TO 'coffee_cashier';
 GRANT EXECUTE ON PROCEDURE coffee_shop_db.sp_pay_order TO 'coffee_cashier';
-
--- пример: создать пользователя-кассира и выдать ему роль
--- CREATE USER 'cashier1'@'localhost' IDENTIFIED BY 'change_me';
--- GRANT 'coffee_cashier' TO 'cashier1'@'localhost';
